3a421a3c94ebe3a1f63eadb723360fc9b773f2e5
yusroh <yusrohpopoola@gmail.com>
2022-04-17 13:51:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-/*main -entry point
- * returns 0 always*/
+/*
+ * main -entry point ,prints phrase
+* returns: 0 always
+* */
 int main(void)
 {
-	printf("with proper grammar , but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
