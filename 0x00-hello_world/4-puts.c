9e48fb0b3c390666c583784c806b547d0b45f991
yusroh <yusrohpopoola@gmail.com>
2022-04-17 09:07:22 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+/*main- prints a sentence*/
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+{
+       	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
