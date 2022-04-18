b5d44d3280d57c8252e9f86f364a2bc6ade140dc
yusroh123 <101255671+yusroh123@users.noreply.github.com>
2022-04-18 22:36:06 +0100

    Fixing errors

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,11 +14,9 @@ void print_numbers(void)
 
 	for (number = '0'; number <= '9'; number++)
 	{ 
-          if (number != 2 || number != 4)
-	{	_putchar(number);
-          Else 
-              Break;
+          if (number != 2 && number != 4)
+		_putchar(number);
 	}
 	_putchar('\n');
 } 
-}
+
