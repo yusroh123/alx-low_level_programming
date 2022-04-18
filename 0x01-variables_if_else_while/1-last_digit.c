fdabf8e5f3d63d9e9f2872389280c5562413c706
yusroh123 <101255671+yusroh123@users.noreply.github.com>
2022-04-18 11:36:02 +0100

    Resolved some issues

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
 /* 
 * main - Prints the last number of n
+*Description:tells whether it is greater,less,or equal to 0
 *Return: 0 if true
- */
+*/
 int main(void)
 {
         int n;
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d", n, n % 10);
 if (n % 10 > 5)
 {printf(" and is greater than 5\n"); }
 else if (n % 10 == 0)
-{printf(" and is zero\n"); }
+{printf(" and is 0\n"); }
 else if (n % 10 < 6 && n % 10 != 0)
 {printf(" and is less than 6 and not 0\n"); }
 	return (0);
