fdb069b0838c40a52887be3cd68d2d37fb66016b
yusroh123 <101255671+yusroh123@users.noreply.github.com>
2022-04-18 21:28:41 +0100

    My personal header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef ALX
+#define ALX
+
+/*
+* File:alx
+* Author: Bostone
+* Description:This is file containing all declations of all functions
+*/
+int _putchar(char num);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int isPrime(long int n);
+long int biggestFactor(long int a);
+void print_number(int n);
+int numDigits(int n);
+#endif
