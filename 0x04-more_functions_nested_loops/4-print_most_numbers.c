1ba30d8c473ceb580bc6773238f6b26d3dccd7a6
yusroh123 <101255671+yusroh123@users.noreply.github.com>
2022-04-18 22:24:59 +0100

    Added logical operator

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,8 @@ void print_numbers(void)
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
-	{
+	{ 
+          if (number != 2 || number != 4)
 		_putchar(number);
 	}
 	_putchar('\n');
