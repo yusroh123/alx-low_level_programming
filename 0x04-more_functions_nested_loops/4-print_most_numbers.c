22f57791ad45f997f4f4dca2ea18644363d03671
yusroh123 <101255671+yusroh123@users.noreply.github.com>
2022-04-18 22:38:10 +0100

    Recreating file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_numbers - entry point
+ * print_most_numbers - entry point
  *
  * Description: Prints the numbers with _putchar
  *
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
-	{ 
-          if (number != 2 && number != 4)
-		_putchar(number);
+	{
+		if (number == '2' || number == '4')
+			;
+		else
+			_putchar(number);
 	}
 	_putchar('\n');
-} 
-
+}
