58e94e0fd89ecbc0725841fba12eb33004f7c6ac
yusroh123 <101255671+yusroh123@users.noreply.github.com>
2022-04-18 16:44:15 +0100

    Prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int times = 0;
+	char letter = 'a';
+
+	while (times < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		times++;
+	}
+}
